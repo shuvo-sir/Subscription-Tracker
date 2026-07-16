9dc1ea43544226ae60b2b6f5257a221b6c697067
Shuvo Halder <shuvohalder499@gmail.com>
2026-07-16 23:50:53 +0600

    Implement the arcjet for rate limit

--- config/env.js
+++ config/env.js
@@ -5,6 +5,7 @@ config({path: `.env.${process.env.NODE_ENV  || 'development'}.local`});
 export const {
     PORT, NODE_ENV,
     DB_URL,
-    JWT_SECRET, JWT_EXPIRE
+    JWT_SECRET, JWT_EXPIRE,
+    ARCJET_KEY, ARCJET_ENV
 } = process.env;
 
