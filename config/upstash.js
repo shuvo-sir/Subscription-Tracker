9ea3c6842c76e6ebe681fc36703927d27554e404
Shuvo Halder <shuvohalder499@gmail.com>
2026-07-19 22:27:12 +0600

    commit

--- config/upstash.js
+++ config/upstash.js
@@ -2,7 +2,7 @@ import { Client as WorkflowClient } from "@upstash/workflow";
 
 import { QSTASH_URL, QSTASH_TOKEN } from "./env.js";
 
-
+// Create a new instance of the WorkflowClient with the provided base URL and token. This client will be used to interact with Upstash's workflow services.
 export const workflowClient = new WorkflowClient({
     baseUrl: QSTASH_URL,
     token: QSTASH_TOKEN
